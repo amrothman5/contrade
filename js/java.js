30836f7959bf78e139870a53477d343475745bd9
amrothman5 <amrothman5@gmail.com>
2021-07-13 11:37:04 +0200

    first commit

---
+++ js/java.js
@@ -0,0 +1,46 @@
+var emg =document.getElementById("img1");
+function scale( ) {
+   if(emg.webkitRequestFullscreen){
+    emg.webkitRequestFullscreen();
+   }
+}
+var emg1 =document.getElementById("img2");
+function scale1( ) {
+   if(emg1.webkitRequestFullscreen){
+    emg1.webkitRequestFullscreen( );
+   }
+}
+
+var emg2 =document.getElementById("img3");
+function scale2( ) {
+   if(emg2.webkitRequestFullscreen){
+    emg2.webkitRequestFullscreen( );
+   }
+}
+
+var emg3 =document.getElementById("img4");
+function scale3( ) {
+   if(emg3.webkitRequestFullscreen){
+    emg3.webkitRequestFullscreen( );
+   }
+}
+var emg4 =document.getElementById("img5");
+function scale4( ) {
+   if(emg4.webkitRequestFullscreen){
+    emg4.webkitRequestFullscreen( );
+   }
+}
+var emg5=document.getElementById("img6");
+function scale5( ) {
+   if(emg5.webkitRequestFullscreen){
+    emg5.webkitRequestFullscreen( );
+   }
+}
+
+
+var photo =document.getElementsByClassName("photo1");
+function funphoto( ) {
+   if(photo.webkitRequestFullscreen){
+      photo.webkitRequestFullscreen( );
+   }
+}
