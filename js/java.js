8844b2ec28e7c56658437322fdaa934943288b78
amrothman5 <amrothman5@gmail.com>
2021-08-18 12:32:09 +0200

    first commit

--- js/java.js
+++ js/java.js
@@ -50,11 +50,43 @@ function Banks( ) {
     move2.classList.add("move2");
    h1.innerHTML="";
    h1left.innerHTML="";
-   h1.innerHTML=" <p class='animate__animated  animate__backInRight  animate__delay-1s  right-h fontright  ' > Projects Of Banks  </p>";
+   h1.innerHTML=" <p class='animate__animated  animate__backInRight  animate__delay-1s  right-h fontright  '  style='Cambria, Cochin; color: rgb(214, 30, 30);'> Projects Of Banks  </p>";
    h3.innerHTML=" ";
-   h3.innerHTML = "<p class='animate__animated  animate__backInRight  animate__delay-1s   right-h  fontright2' > Contrade Engineers & Contractors  </p>"
+   h3.innerHTML = "<p class='animate__animated  animate__backInRight  animate__delay-1s   right-h  fontright2'  style='Cambria, Cochin; color: rgb(214, 30, 30);' > Contrade Engineers & Contractors  </p>"
    h4.innerHTML="";
-   h4.innerHTML="<p class='animate__animated  animate__backInRight  animate__delay-1s  right-h ' >Projects  </p>";
+   h4.innerHTML="<p class='animate__animated  animate__backInRight  animate__delay-1s  right-h '  style='Cambria, Cochin; color: rgb(214, 30, 30);' >Projects  </p>";
+   h3left2.innerHTML=``;
+   h3left.innerHTML=``;
+   
+   icon.innerHTML="<p class='animate__animated  animate__backOutLeft      animate__slower 200000  ; '></p>"
+   h3left.appendChild(banks);
+   banks.classList.remove("beforlast3");
+   banks.classList.add("beforlast3-1");
+}
+// ================= Reverce Funcation=================
+function Banks2( ) {
+   var move1 = document.getElementById("box");
+   var move2 = document.getElementById("box2");
+   var box = document.getElementById("box");
+   var h1 =  document.getElementById("h1");
+   var h3left = document.getElementById("h3left");
+   var h3left2 = document.getElementById("h3left2");
+   var h3 = document.getElementById("h3");
+   var h4 = document.getElementById("h33");
+   var h1left = document.querySelector("#h1left");
+    var icon = document.getElementById("icon");
+    var banks = document.getElementById("banks");
+    move1.classList.remove("move1");
+    move2.classList.remove("move2");
+    move1.classList.add("move22");
+    move2.classList.add("move11");
+   h1.innerHTML="";
+   h1left.innerHTML="";
+   h1.innerHTML=" ";
+   h3.innerHTML=" ";
+   h3.innerHTML = ""
+   h4.innerHTML="";
+   h4.innerHTML="";
    h3left2.innerHTML=``;
    h3left.innerHTML=``;
    
